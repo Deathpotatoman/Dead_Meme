7456e61af08ba55e4a7103378515e0e72b096246
Deathpotatoman <35238891+Deathpotatoman@users.noreply.github.com>
2018-01-09 09:52:42 -0600

    Add files via upload

--- Dead_Meme.java
+++ Dead_Meme.java
@@ -1,5 +1,14 @@
-
-public class Dead_Meme
+import math.java;
+import javax.swing.*;
+public class Dead_Meme extends JFrame
     {
-    protected double multiplier;//this double is the multi
+    protected double multiplier;//this double is the multiplier for dead meme generators
+    protected double create;//this double tracks how much each dead meme generates
+    protected int total;//this tracks how many of each generator is
+    protected double price;//tracks price of each generator
+    void upgradePrice()
+        {
+        price=round(price*total*1.15);
+        }
+        
     }
